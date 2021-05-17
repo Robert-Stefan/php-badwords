bd3e134651ab26a49d2c759a84dc59c1705d8f19
Robert-Stefan <robertboss05@hotmail.it>
2021-05-17 14:48:10 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<!--  DESCRIZIONE:
+Creare una variabile con assegnato un testo per popolare un paragrafo.
+Visualizzare a schermo il paragrafo.
+Visualizzare a schermo la lunghezza di testo del paragrafo.
+Passare un parametro (query string) nella url chiamato badword con assegnato il valore di una parola scelta da voi, ottenere il suo valore con GET in PHP e sostituire ogni ricorrenza di questa parola, all'interno del testo creato da voi sopra, con tre *.
+Stampare a schermo il paragrafo con il testo censurato. -->
+
+</body>
+</html>
